793415c9b6aee24e55d70b6f3eb54386f91e88eb
kumar harsh <harsh@kumars-MacBook-Air.local>
2023-04-30 22:18:10 +0530

    Added demo code for implementations

---
+++ server/main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import requests
+import firebase
+import json
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def status():
+    cars = get_most_recent_cars()
+    return render_template('status.html', data = cars)
+
+@app.route('/simulator')
+def simulator():
+    return render_template('simulator.html')
+
+def get_most_recent_cars():    
+    cars = {}
+    all_history = firebase.get('/history', None)
+
+    # Get all from prev_station, group by car_id and then get the most recent timestamp for each car
+
+    for key, value in all_history.items():
+        if value["station_id"] == PREV_STATION:
+            try:
+                if cars[value["car_id"]] and cars[value["car_id"]]["timestamp"] < value["timestamp"]:
+                    # Same car_id but newer data
+                    cars[value["car_id"]] = value
+            except:
+                cars[value["car_id"]] = value
+
+    for key, value in cars.items():
+        print (key, value)
+
+    return cars
+
+@socketio.on('trigger_camera')
+def trigger_camera():
+    print('trigger_camera received, broadcasting')
+    emit('trigger_camera', broadcast=True)
+
+@socketio.on('update_ui')
+def update_ui():
+    print('update ui in main')
+    emit('update_ui_for_browser', broadcast=True)
+
+if __name__ == '__main__':
+    PREV_STATION = 1
+    MY_STATION_ID = 2
+    firebase = firebase.FirebaseApplication('https://harsh.firebaseio.com', None)
+    socketio.run(app)
---
+++ train_car_pi_code.py
@@ -0,0 +1,117 @@
+import argparse
+import picamera
+import pyaudio
+import time
+import wave
+import StringIO
+from picotts import PicoTTS
+from google.cloud import vision
+from google.cloud.vision import types
+from PIL import Image, ImageDraw
+from firebase import firebase
+from socketIO_client import SocketIO, LoggingNamespace
+
+firebase = firebase.FirebaseApplication('https://harsh.firebaseio.com', None)
+camera = picamera.PiCamera()
+picotts = PicoTTS()
+
+SERVER = ''
+PORT = 80
+TRAIN_ID = 10011
+CAR_ID = 0
+
+STATION_LIST = [0,1,2,3]
+
+station_index = 1
+
+def playSound():
+    global picotts
+    wavs = picotts.synth_wav('Stand clear of the closing doors please.');
+    wav = wave.open(StringIO.StringIO(wavs))
+    p = pyaudio.PyAudio()  
+    stream = p.open(format = p.get_format_from_width(wav.getsampwidth()),  
+                    channels = wav.getnchannels(),  
+                    rate = wav.getframerate(),  
+                    output = True)  
+    data = wav.readframes(1024)  
+
+    while data:  
+        stream.write(data)  
+        data = wav.readframes(1024)  
+
+    stream.stop_stream()  
+    stream.close()  
+
+    p.terminate()  
+
+
+def takephoto():
+    camera.capture('image.jpg')
+
+def detect_face(face_file, max_results=4):
+    client = vision.ImageAnnotatorClient()
+
+    content = face_file.read()
+    image = types.Image(content=content)
+
+    return client.face_detection(image=image).face_annotations
+
+
+def highlight_faces(image, faces, output_filename):
+    im = Image.open(image)
+    draw = ImageDraw.Draw(im)
+
+    for face in faces:
+        box = [(vertex.x, vertex.y)
+               for vertex in face.bounding_poly.vertices]
+        draw.line(box + [box[0]], width=5, fill='#00ff00')
+
+    im.save(output_filename)
+    
+def on_connect():
+    print('Connected')
+
+def on_disconnect():
+    print('Disconnected')
+
+def on_reconnect():
+    print('Reconnected')
+
+def on_triggerCamera(*args):
+    print(args)
+    global station_index
+    print('Detecting train riders...')
+    takephoto()
+    with open('image.jpg', 'rb') as image:
+        playSound()
+        faces = detect_face(image, 50)
+        print('Found {} face{}'.format(
+            len(faces), '' if len(faces) == 1 else 's'))
+
+        print('Writing to file {}'.format('output.jpg'))
+
+        image.seek(0)
+        # highlight_faces(image, faces, 'output.jpg')
+        train_car = {
+                'timestamp' : time.time(),
+                'train_id' : TRAIN_ID,
+                'car_id' : CAR_ID,
+                'how_full' : len(faces),
+                'station_id' : STATION_LIST[station_index]
+        }
+
+        result = firebase.post('/history', train_car)
+        print result
+        # station_index = (station_index + 1) % 4
+        print('Moving to station ' + str(station_index))
+        socketIO.emit('update_ui')
+        print('Emitted update_ui')
+
+print('Client is running, listening for commands')
+socketIO = SocketIO(SERVER, PORT, LoggingNamespace)
+socketIO.on('connect', on_connect)
+socketIO.on('disconnect', on_disconnect)
+socketIO.on('reconnect', on_reconnect)
+socketIO.on('trigger_camera', on_triggerCamera)
+socketIO.wait(seconds=10000)
+
